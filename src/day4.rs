c325f4fdc645fdeeedb9cc9545e7046956e37337
iyaja <ajayuppili@gmail.com>
2019-12-04 00:59:38 -0600

    feat: day 4

---
+++ src/day4.rs
@@ -0,0 +1,127 @@
+use std::collections::HashMap;
+
+#[aoc_generator(day2)]
+pub fn input_generator(input: &str) -> Vec<usize> {
+    input
+        .split("-")
+        .map(|l| {
+            l.parse::<usize>().unwrap()
+        })
+        .collect()
+}
+
+#[aoc(day4, part1)]
+pub fn check_simple(input: &[u8]) -> usize {
+    let mut valid = 0;
+    
+    for trial in 372037..905157 {
+        let pass = trial.to_string();
+        let mut doubleCount = 0;
+        for i in 1..pass.len() {
+            if pass.chars().nth(i) == pass.chars().nth(i - 1) {
+                doubleCount += 1;
+            }
+        }
+
+        if doubleCount == 0 {
+            continue;
+        }
+
+        let mut ascending = true;
+        for i in 1..pass.len() {
+            if pass.chars().nth(i) < pass.chars().nth(i - 1) {
+                ascending = false
+            }
+        }
+        if !ascending {
+            continue;
+        }
+
+        valid += 1;
+    }
+    valid
+}
+
+#[aoc(day4, part2)]
+pub fn check(input: &[u8]) -> usize {
+    let mut valid = 0;
+    for trial in 372037..905157 {
+        let pass = trial.to_string();
+        // let mut doubleCount = 0;
+        // for i in 1..pass.len() {
+        //     if pass.chars().nth(i) == pass.chars().nth(i - 1) {
+        //         doubleCount += 1;
+        //     }
+        // }
+
+        // if doubleCount % 2  != 0 {
+        //     continue;
+        // }
+
+        // let mut ascending = true;
+        // for i in 1..pass.len() {
+        //     if pass.chars().nth(i) < pass.chars().nth(i - 1) {
+        //         ascending = false
+        //     }
+        // }
+        // if !ascending {
+        //     continue;
+        // }
+        if (validate(&pass)) {
+            valid += 1;
+        }
+        
+    }
+    valid
+}
+
+pub fn validate(input: &str) -> bool {
+    
+    let pass = input.to_string();
+    let mut counts = 1;
+    let mut idx = 0;
+    let mut got_double = false;
+    for mut _i in 0..pass.len()-1 {
+        //println!("old idx is {}", idx);
+        for j in 1..pass.len() {
+            //println!("here at i={}, j={}", idx, j);
+            if pass.chars().nth(idx) == pass.chars().nth(idx + j) {
+                counts += 1;
+            } else {
+                break;
+            }
+        }
+        if counts == 2 {
+          got_double = true;
+        }
+        
+        idx+=counts;
+        //println!("new idx is {}", idx);
+        //println!("{}", counts);
+        counts = 1;
+        if idx >= pass.len()-1 {
+          break;
+        }
+    }
+    let mut doubleCount = 0;
+    for i in 1..pass.len() {
+            if pass.chars().nth(i) == pass.chars().nth(i - 1) {
+                doubleCount += 1;
+            }
+        }
+
+    if doubleCount == 0 {
+        return false;
+    }
+
+    let mut ascending = true;
+    for i in 1..pass.len() {
+        if pass.chars().nth(i) < pass.chars().nth(i - 1) {
+            ascending = false
+        }
+    }
+    ascending && got_double
+}
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,9 @@ extern crate aoc_runner_derive;
 // extern crate crypto;
 
 // pub mod day1;
-pub mod day2;
+// pub mod day2;
 // pub mod day3;
-// pub mod day4;
+pub mod day4;
 // pub mod day5;
 
 aoc_lib!{ year = 2019 }
